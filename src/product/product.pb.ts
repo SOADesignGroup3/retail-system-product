f7f87ea0658d0cfcdca3b47a5c646ccbeceb1083
Nabila070 <nabilabellosani@gmail.com>
2023-08-18 06:35:06 +0100

    message changed _

--- src/product/product.pb.ts
+++ src/product/product.pb.ts
@@ -1,8 +1,8 @@
 /* eslint-disable */
-import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
-import { Observable } from 'rxjs';
+import { GrpcMethod, GrpcStreamMethod } from "@nestjs/microservices";
+import { Observable } from "rxjs";
 
-export const protobufPackage = 'product';
+export const protobufPackage = "product";
 
 export interface CreateProductRequest {
   name: string;
@@ -47,67 +47,41 @@ export interface DecreaseStockResponse {
   message: string;
 }
 
-export const PRODUCT_PACKAGE_NAME = 'product';
+export const PRODUCT_PACKAGE_NAME = "product";
 
 export interface ProductServiceClient {
-  createProduct(
-    request: CreateProductRequest,
-  ): Observable<CreateProductResponse>;
+  createProduct(request: CreateProductRequest): Observable<CreateProductResponse>;
 
   findOne(request: FindOneRequest): Observable<FindOneResponse>;
 
-  decreaseStock(
-    request: DecreaseStockRequest,
-  ): Observable<DecreaseStockResponse>;
+  decreaseStock(request: DecreaseStockRequest): Observable<DecreaseStockResponse>;
 }
 
 export interface ProductServiceController {
   createProduct(
     request: CreateProductRequest,
-  ):
-    | Promise<CreateProductResponse>
-    | Observable<CreateProductResponse>
-    | CreateProductResponse;
+  ): Promise<CreateProductResponse> | Observable<CreateProductResponse> | CreateProductResponse;
 
-  findOne(
-    request: FindOneRequest,
-  ): Promise<FindOneResponse> | Observable<FindOneResponse> | FindOneResponse;
+  findOne(request: FindOneRequest): Promise<FindOneResponse> | Observable<FindOneResponse> | FindOneResponse;
 
   decreaseStock(
     request: DecreaseStockRequest,
-  ):
-    | Promise<DecreaseStockResponse>
-    | Observable<DecreaseStockResponse>
-    | DecreaseStockResponse;
+  ): Promise<DecreaseStockResponse> | Observable<DecreaseStockResponse> | DecreaseStockResponse;
 }
 
 export function ProductServiceControllerMethods() {
   return function (constructor: Function) {
-    const grpcMethods: string[] = ['createProduct', 'findOne', 'decreaseStock'];
+    const grpcMethods: string[] = ["createProduct", "findOne", "decreaseStock"];
     for (const method of grpcMethods) {
-      const descriptor: any = Reflect.getOwnPropertyDescriptor(
-        constructor.prototype,
-        method,
-      );
-      GrpcMethod('ProductService', method)(
-        constructor.prototype[method],
-        method,
-        descriptor,
-      );
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod("ProductService", method)(constructor.prototype[method], method, descriptor);
     }
     const grpcStreamMethods: string[] = [];
     for (const method of grpcStreamMethods) {
-      const descriptor: any = Reflect.getOwnPropertyDescriptor(
-        constructor.prototype,
-        method,
-      );
-      GrpcStreamMethod('ProductService', method)(
-        constructor.prototype[method],
-        method,
-        descriptor,
-      );
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod("ProductService", method)(constructor.prototype[method], method, descriptor);
     }
   };
 }
 
-export const PRODUCT_SERVICE_NAME = 'ProductService';
+export const PRODUCT_SERVICE_NAME = "ProductService";
